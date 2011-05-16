a3cca4504df9672ad7aa73c21cff94faebccc117
Andrew Farmer <anfarmer@ku.edu>
2011-05-15 23:52:26 -0500

    Better comments

--- Bayes.hs
+++ Bayes.hs
@@ -17,30 +17,67 @@ newtype (HSet s) => Network s = Network { dists :: s }
 -- heterogenous set of observations makes up the evidence
 newtype (HSet s, Variable s) => Evidence s = Evidence s
     deriving (Show)
+-- list of probabilities, with heterogeneous sets as keys
+newtype (HSet vs, Variable vs) => Factor vs = Factor [(vs,Float)]
+    deriving (Show)
+
+noEvidence = Evidence HTip
+
+-- The main function
+elim :: ( Variable x, Variable e, HNotMember x e
+        , HFoldr FoldWithVarsHCUnion HTip n s
+        , HDiff s (HAdd x e) hs
+        , HFoldr (FoldNetworkToFactors e) HTip n fs
+        , HFoldr SumoutHiddens fs hs fs'
+        , Variable s', HSet s'
+        , HFoldr FoldPointwise (Factor HTip) fs' (Factor s')
+        , HSubtype s' (Singleton x), HMember x s')
+     => x -> Evidence e -> Network n -> HP (Singleton x)
+elim x (Evidence e) n = toHP $ foldPointwise reduced
+    where -- note: it's probably wrong to have the factor list be
+          -- type-indexed. Should be a normal HList. Reason: intermediate
+          -- factors generated during the summing out process could have
+          -- the same types, but will get combined immediately. This
+          -- causes some queries to fail at compile time erroneously
+          --
+          -- factors :: fs
+          factors = hFoldr (FoldNetworkToFactors e) hEmpty (dists n)
+          -- hiddens :: hs
+          hiddens = (vars n) `hDiff` (x .>. e)
+          -- reduced :: fs'
+          reduced = hFoldr SumoutHiddens factors hiddens
+
+-- | Remove evidence (via hProject) and normalize
+toHP :: (Variable a, HSet a, HSubtype a a') => Factor a -> HP a'
+toHP (Factor as) = HC (\htip -> normalize [(hProject a,p) | (a,p) <- as])
+
+-- Find the vars in a network.
+vars :: (HFoldr FoldWithVarsHCUnion HTip n s') => Network n -> s'
+vars n = hFoldr FoldWithVarsHCUnion HTip (dists n)
 
 data FoldWithVarsHCUnion = FoldWithVarsHCUnion
 instance ( HUnion a b s', HSet s', HUnion s s' s'')
         => HFoldOp FoldWithVarsHCUnion (HC a b) s s'' where
     hFoldOp _ e s = s .+. (varsHC e)
 
-vars :: (HFoldr FoldWithVarsHCUnion HTip n s') => Network n -> s'
-vars n = hFoldr FoldWithVarsHCUnion HTip (dists n)
-
 -- Holy context explosion batman!
+-- Convert a network (list of conditional probabilities to a list of factors
 data FoldNetworkToFactors es = FoldNetworkToFactors es
 instance ( Variable a, Variable b, Variable vs, Variable es, Variable a', Variable b'
          , HUnion a b vs
-         , HIntersection es a s', HUnion s' a a', HIso a' a
-         , HIntersection es b s'', HUnion s'' b b', HIso b' b
+         , HIntersection es a s', HUnion s' a a', HSubtype a' a
+         , HIntersection es b s'', HUnion s'' b b', HSubtype b' b
          , HEqual a a, HEqual b b, HNotMember (Factor vs) s
          , HSet es, HSet s', HSet s'', HSet vs)
         => HFoldOp (FoldNetworkToFactors es) (HC a b) s (HAdd (Factor vs) s) where
     hFoldOp (FoldNetworkToFactors es) hc factors = (mkFactor hc (Evidence es)) .>. factors
 
+-- Find only those factors that contain a hidden variable
 data Contains h = Contains h
 instance (HElem h s b) => HFilterOp (Contains h) (Factor s) b where
     hFilterOp _ _ = fst $ hElem (undefined :: h) (witness :: s)
 
+-- Used when summing out a variable to 'set' the variable to a specific value
 data SetAll a = SetAll a
 instance (Variable a, Variable as, HSet as, HElem a as b, HNotMember (Factor as) s)
         => HFoldOp (SetAll a) (Factor as) s (HAdd (Factor as) s) where
@@ -49,6 +86,7 @@ instance (Variable a, Variable as, HSet as, HElem a as b, HNotMember (Factor as)
 set :: (Variable a, Variable as, HSet as, HBool b, HElem a as b) => a -> Factor as -> Factor as
 set a (Factor as) = Factor [ (v,p) | (v,p) <- as, let (tf,a') = hElem a v, true tf, a == a' ]
 
+-- Sum out a hidden variable.
 data SumoutHiddens = SumoutHiddens
 instance ( Variable h, Variable s, Variable s', HSet fs, HSet fs'
          , HFilter (Contains h) fs fs', HDiff fs fs' fs''
@@ -60,6 +98,8 @@ instance ( Variable h, Variable s, Variable s', HSet fs, HSet fs'
         where fs' = hFilter (Contains h) fs -- factors that contain h
               fs'' = hDiff fs fs'           -- factors that don't contain h
 
+-- for every possible value of the hidden variable, compute the pointwise project
+-- of the factors containing that hidden variable, then add them together
 sumout :: forall h fs fs' s s'.
            ( Variable h, Variable s, Variable s'
            , HSet fs
@@ -71,32 +111,7 @@ sumout h fs = foldr1 pointwiseA withouth
     where withouth = [ Factor [ (hDelete h v,p) | (v,p) <- f ] | Factor f <- products ]
           products = [ foldPointwise $ hFoldr (SetAll v) hEmpty fs | v <- domain :: [h]]
 
-elim :: ( Variable x, Variable e, HNotMember x e
-        , HFoldr FoldWithVarsHCUnion HTip n s
-        , HDiff s (HAdd x e) hs
-        , HFoldr (FoldNetworkToFactors e) HTip n fs
-        , HFoldr SumoutHiddens fs hs fs'
-        , Variable s', HSet s'
-        , HFoldr FoldPointwise (Factor HTip) fs' (Factor s')
-        , Variable s'', HSet s''
-        , HDiff s' e s'')
-     => x -> Evidence e -> Network n -> HP s''
-elim x (Evidence e) n = toHP $ rmEvidence (Evidence e) $ foldPointwise reduced
-    where -- factors :: fs
-          factors = hFoldr (FoldNetworkToFactors e) hEmpty (dists n)
-          -- hiddens :: hs
-          hiddens = (vars n) `hDiff` (x .>. e)
-          -- reduced :: fs'
-          reduced = hFoldr SumoutHiddens factors hiddens
-
-toHP :: (Variable a, HSet a) => Factor a -> HP a
-toHP (Factor as) = HC (\htip -> normalize as)
-
-rmEvidence :: ( Variable e, Variable ea, Variable a
-              , HSet e, HSet ea, HSet a, HDiff ea e a)
-           => Evidence e -> Factor ea -> Factor a
-rmEvidence (Evidence e) (Factor eas) = Factor [(hDiff ea e,p) | (ea,p) <- eas]
-
+-- fold the factors with pointwise multiplication
 data FoldPointwise = FoldPointwise
 instance ( Variable bs, Variable abs, Variable as
          , HUnion as bs abs, HIntersection bs as s', HIntersection as bs s, HEqual s s'
@@ -105,26 +120,26 @@ instance ( Variable bs, Variable abs, Variable as
     hFoldOp _ as bs = pointwiseM as bs
 foldPointwise fs = hFoldr FoldPointwise pointwiseMident fs
 
--- list of probabilities, with heterogeneous sets as keys
-newtype (HSet vs, Variable vs) => Factor vs = Factor [(vs,Float)]
-    deriving (Show)
-
-conditionOn :: forall e v s v'.
-               (HSet e, Variable e, HSet v, HIntersection e v s, HUnion s v v', HIso v' v)
-                 => Evidence e -> v -> v
-conditionOn (Evidence e) v = hReorder $ (e .*. v) .+. v
-
+-- Turn a conditional distribution into a factor
 mkFactor :: forall a b e s s' a' b' vs.
             ( Variable a, Variable b, Variable e, Variable a', Variable b'
             , HSet a, HSet b, HSet e, HSet s, HSet s', HSet a', HSet b'
-            , HIntersection e a s, HUnion s a a', HIso a' a, HEqual a a
-            , HIntersection e b s', HUnion s' b b', HIso b' b, HEqual b b
+            , HIntersection e a s, HUnion s a a', HSubtype a' a, HEqual a a
+            , HIntersection e b s', HUnion s' b b', HSubtype b' b, HEqual b b
             , Variable vs, HSet vs, HUnion a b vs)
          => HC a b -> Evidence e -> Factor vs
 mkFactor (HC f) e = Factor [(i .+. o,p) | i <- nubBy (.==.) $ map (conditionOn e) (domain :: [a])
                                         , (o,p) <- f i
                                         , o .==. (conditionOn e o)]
 
+-- condition entries on the evidence values
+conditionOn :: forall e v s v'.
+               (HSet e, Variable e, HSet v, HIntersection e v s, HUnion s v v', HSubtype v' v)
+                 => Evidence e -> v -> v
+conditionOn (Evidence e) v = hProject $ (e .*. v) .+. v
+
+-- pointwise multiplication of factors (see pg 510 of AI: A Modern Approach 2nd ed)
+-- a kind of union-based multiplication
 pointwiseM :: ( HSet as, HSet bs, HSet abs
               , Variable as, Variable bs, Variable abs
               , HIntersection as bs s, HIntersection bs as s', HEqual s s'
@@ -135,9 +150,11 @@ pointwiseM (Factor as) (Factor bs) = Factor [ (a .+. b, p * q)
                                             , (b,q) <- bs
                                             , (a .*. b) .==. (b .*. a) ]
 
+-- identity for fold of pointwise multiplication
 pointwiseMident :: Factor HTip
 pointwiseMident = Factor [(hEmpty,1.0)]
 
+-- pointwise addition of two factors, used in summing out process
 pointwiseA :: (HSet as, Variable as, HEqual as as)
            => Factor as -> Factor as -> Factor as
 pointwiseA (Factor as) (Factor bs) = Factor [ (a, p + q)
--- HSet.hs
+++ HSet.hs
@@ -2,47 +2,80 @@
              FlexibleInstances, TypeFamilies, UndecidableInstances, OverlappingInstances #-}
 module HSet where
 
+---------- HSet Basics --------------
+data HTip = HTip         deriving (Eq)
+data HAdd e s = HAdd e s deriving (Eq)
+
+type e :>: s = HAdd e s
+
+class HSet s where witness :: s
+
+instance HSet HTip where witness = HTip
+instance HSet s => HSet (HAdd e s) where witness = HAdd (error "witness" :: e) (witness :: s)
+
+class HSet s => HShow s where hshow :: s -> String
+instance HShow HTip where hshow _ = "}"
+instance (Show e, HShow s) => HShow (HAdd e s) where hshow (HAdd e s) = "," ++ show e ++ hshow s
+
+instance Show HTip where
+    show _ = "{}"
+
+instance (Show a, HShow s) => Show (HAdd a s) where
+    show (HAdd e s) = "{" ++ show e ++ hshow s
+
+infixr .>.
+(.>.) :: (HSet s, HNotMember e s) => e -> s -> e :>: s
+(.>.)  = HAdd
+
+type Singleton a = a :>: HTip
+singleton :: e -> Singleton e
+singleton = (.>. HTip)
+hEmpty = HTip
+
+-- test = (42 :: Int) .>. "hi" .>. ('a',12::Int) .>. HTip
+
+---------- HSet API ------------------
 class Error x
 data TypeNotFound e
 data TypeFound e
 
--- | Type level reordering. Values in s are put in same order as s'
--- Values whose types are not in s' are discarded.
--- Values whose types are not in s will trigger an error at typechecking time.
--- This is necessary to apply functions that take HSets as arguments
-apphfn :: forall s' b s. (HSet s, HIso s s') => (s' -> b) -> s -> b
-apphfn f = f . hReorder
+-- | In order to handle the fact that HSet's are actually type-indexed
+-- HLists (they have an ordering), we use hProject to properly reorder
+-- arguments to a function
+apphfn :: forall s' b s. (HSet s, HSubtype s s') => (s' -> b) -> s -> b
+apphfn f = f . hProject
 
-class (HSet s, HSet s') => HIso s s' where hReorder :: s -> s'
-instance (HSet s) => HIso s HTip where hReorder _ = HTip
-instance (HMember e s, HSet s', HIso s s') => HIso s (HAdd e s') where
-    hReorder s = HAdd (hMember s) (hReorder s)
+-- | Subtyping: s is a subtype of s' if s includes every type in s'
+-- Value-level function hProject will project values out of s in the
+-- same order demanded by the type of s'. This is necessary to apply
+-- functions that take HSets as arguments
+class (HSet s, HSet s') => HSubtype s s' where hProject :: s -> s'
+instance (HSet s) => HSubtype s HTip where hProject _ = HTip
+instance (HMember e s, HSet s', HSubtype s s') => HSubtype s (HAdd e s') where
+    hProject s = HAdd (hMember s) (hProject s)
 
+-- | Membership assertion. Fails at compile time if e is not present in s
 class (HSet s) => HMember e s where hMember :: s -> e
 instance Error (TypeNotFound e) => HMember e HTip where hMember _ = undefined
 instance (HSet s, HNotMember e s) => HMember e (HAdd e s) where hMember (HAdd e _) = e
 instance (HSet s, HMember e s) => HMember e (HAdd e' s) where hMember (HAdd _ s) = hMember s
 
+-- | Asserts e is not a member of s. Solely a type-level constraint.
 class (HSet s) => HNotMember e s
 instance HNotMember e HTip
 instance (HNotMember e s) => HNotMember e (HAdd e' s)
 instance (Error (TypeFound e), HSet s) => HNotMember e (HAdd e s)
 
--- | Type level difference, all values in first set whose types are present in second set are removed
+-- | Type level difference, all values in s whose types are present in s' are removed
 class HDiff s s' s'' | s s' -> s'' where hDiff :: s -> s' -> s''
 instance HDiff s HTip s where hDiff = const
 instance (HDelete e s s'', HDiff s'' s' s''') => HDiff s (HAdd e s') s''' where hDiff s (HAdd e s') = hDiff (hDelete e s) s'
 
--- | Type level union, giving preference to values present in the first set.
+-- | Type level union, giving preference to values present in s.
 infixl .+.
 (.+.) :: (HUnion s s' s'') => s -> s' -> s''
 (.+.) = hUnion
 
-{-
--- Why does this force us to propagate the HDiff and HMerge constraints in some places but not others?
-class HUnion s s' s'' | s s' -> s'' where hUnion :: s -> s' -> s''
-instance (HDiff s' s d, HMerge s d s'') => HUnion s s' s'' where hUnion s s' = hMerge s (hDiff s' s)
--}
 class HUnion s s' s'' | s s' -> s'' where hUnion :: s -> s' -> s''
 instance HUnion s HTip s where hUnion = const
 instance (HElem e s b, HUnionCase b s e s' s'') => HUnion s (HAdd e s') s'' where hUnion s (HAdd e s') = hUnionCase (fst $ hElem e s) s e s'
@@ -51,15 +84,15 @@ class (HBool b) => HUnionCase b s e s' s'' | b s e s' -> s'' where hUnionCase ::
 instance (HUnion (HAdd e s) s' s'') => HUnionCase HFalse s e s' s'' where hUnionCase _ s e s' = hUnion (HAdd e s) s'
 instance (HUnion s s' s'') => HUnionCase HTrue s e s' s'' where hUnionCase _ s _ s' = hUnion s s'
 
--- | Type level intersection, keeping values present in the first set.
+-- | Type level intersection, keeping values present in s.
+infixl .*.
+(.*.) :: (HIntersection s s' s'') => s -> s' -> s''
+(.*.) = hIntersection
+
 class HIntersection s s' s'' | s s' -> s'' where
     hIntersection :: s -> s' -> s''
 instance (HIntersection' HTip s s' s'') => HIntersection s s' s'' where
     hIntersection = hIntersection' HTip
-
-infixl .*.
-(.*.) :: (HIntersection s s' s'') => s -> s' -> s''
-(.*.) = hIntersection
 
 class HIntersection' a s s' s'' | a s s' -> s'' where hIntersection' :: a -> s -> s' -> s''
 instance HIntersection' a HTip s' a where hIntersection' acc _ _ = acc
@@ -71,25 +104,27 @@ instance (HIntersection' a s s' s'') => HIntersectionCase HFalse a e s s' s'' wh
 instance (HIntersection' (HAdd e a) s s' s'') => HIntersectionCase HTrue a e s s' s''
     where hIntersectionCase _ a e s s' = hIntersection' (HAdd e a) s s'
 
--- | Type level deletion, deletes any value matching the type of the first argument, which is a witness only
+-- | Type level deletion, deletes any value of type e, which need be a witness only
 class (HSet s, HSet s') => HDelete e s s' | e s -> s' where hDelete :: e -> s -> s'
 instance HDelete e HTip HTip where hDelete _ = id -- empty set
 instance (HSet s) => HDelete e (HAdd e s) s where hDelete _ (HAdd _ s) = s -- element found
 instance (HSet s, HSet s', HDelete e s s', s'' ~ (HAdd e' s')) => HDelete e (HAdd e' s) s'' where -- recursive case
     hDelete e (HAdd e' s) = HAdd e' (hDelete e s)
 
--- | Type level merge, will fail if sets share a type
+-- | Type level merge, will fail if s and s' share a type
 class HMerge s s' s'' | s s' -> s'' where hMerge :: s -> s' -> s''
 instance HSet s' => HMerge HTip s' s' where hMerge _ = id
 instance (HSet s, HSet s', HNotMember e s', HMerge s s' s'') => HMerge (HAdd e s) s' (HAdd e s'') where hMerge (HAdd e s) = HAdd e . (hMerge s)
 
--- | Type level foldr, first argument (function) must have an HFoldOp instance
+-- | Type level foldr, first argument (the function, named by a data type) must have an HFoldOp instance
 class (HSet s) => HFoldr f v s s' | f v s -> s' where hFoldr :: f -> v -> s -> s'
 instance HFoldr f v HTip v where hFoldr _ v _ = v
 instance (HSet s, HFoldr f v s s', HFoldOp f e s' s'') => HFoldr f v (HAdd e s) s'' where hFoldr f v (HAdd e s) = hFoldOp f e (hFoldr f v s)
 
+-- | Each function we wish to use with HFoldr needs an HFoldOp instance
 class HFoldOp f e s s' | f e s -> s' where hFoldOp :: f -> e -> s -> s'
 
+{-
 -- | Experimental HMap in terms of HFoldr
 class HMap a b s s' | a b s -> s' where
     hMap :: (a -> b) -> s -> s'
@@ -98,8 +133,7 @@ instance (HFoldr (HMapOp a b) HTip s s') => HMap a b s s' where
 
 data HMapOp e e' = HMapOp (e -> e')
 instance HFoldOp (HMapOp e e') e s (HAdd e' s) where hFoldOp (HMapOp f) e s = HAdd (f e) s
-
--- hmap f = hFoldr (HMapOp f)
+-}
 
 -- | Type level filter, first argument (function) must have an HFilterOp instance
 class (HSet s) => HFilter f s s' | f s -> s' where hFilter :: f -> s -> s'
@@ -122,8 +156,11 @@ hApply f s = (f a) .>. s'
     where a = hMember s
           s' = hDelete a s
 
+-- for a container of HSets
+hFmap f = fmap (hApply f)
+
 -- | Reverse a HSet (ignore the fact that sets should be unordered!)
--- Depends on an implementation detail of hUnion
+-- Depends on an implementation detail of hUnion, probably shouldn't exist
 hReverse :: (HUnion HTip s s') => s -> s'
 hReverse s = hUnion hEmpty s
 
@@ -160,6 +197,7 @@ infix .==.
 (.==.) :: (HEqual s s') => s -> s' -> Bool
 (.==.) = hEqual
 
+-- Can probably be replaced with some clever usage of HUnion, HIntersection, or HSubtype
 class (HSet s, HSet s') => HEqual s s' where hEqual :: s -> s' -> Bool
 instance HEqual HTip HTip where hEqual _ _ = True
 instance (HSet s) => HEqual HTip (HAdd e s) where hEqual _ _ = False
@@ -169,38 +207,5 @@ class HEqualCase b e s s' where hEqualCase :: e -> (b,e) -> s -> s' -> Bool
 instance HEqualCase HFalse e s s' where hEqualCase _ _ _ _ = False
 instance (Eq e, HDelete e s' s'', HEqual s s'') => HEqualCase HTrue e s s' where hEqualCase e (_,e') s s' = (e == e') && (hEqual s (hDelete e' s'))
 
----------- HSET ----------------------
-data HTip = HTip         deriving (Eq)
-data HAdd e s = HAdd e s deriving (Eq)
 
-type e :>: s = HAdd e s
-
-class HSet s where witness :: s
-
-instance HSet HTip where witness = HTip
-instance HSet s => HSet (HAdd e s) where witness = HAdd (error "witness" :: e) (witness :: s)
-
-class HSet s => HShow s where hshow :: s -> String
-instance HShow HTip where hshow _ = "}"
-instance (Show e, HShow s) => HShow (HAdd e s) where hshow (HAdd e s) = "," ++ show e ++ hshow s
-
-instance Show HTip where
-    show _ = "{}"
-
-instance (Show a, HShow s) => Show (HAdd a s) where
-    show (HAdd e s) = "{" ++ show e ++ hshow s
----------- HSET ----------------------
-
-infixr .>.
-(.>.) :: (HSet s, HNotMember e s) => e -> s -> e :>: s
-(.>.)  = HAdd
-
-type Singleton a = a :>: HTip
-singleton :: e -> Singleton e
-singleton = (.>. HTip)
-hEmpty = HTip
-
-hFmap f = fmap (hApply f)
-
-test = (42 :: Int) .>. "hi" .>. ('a',12::Int) .>. HTip
 
